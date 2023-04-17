32285e3dcd676ea429ffeaf66091d6e26dfdd4e4
Ches613 <zverkovmi@gmail.com>
2023-04-17 21:59:12 +0400

    added collectstatic command to init script

--- init_django.sh
+++ init_django.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo docker compose exec web python manage.py makemigrations
 sudo docker compose exec web python manage.py migrate
+sudo docker compose exec web python manage.py collectstatic
 sudo docker compose exec web python manage.py createsuperuser
